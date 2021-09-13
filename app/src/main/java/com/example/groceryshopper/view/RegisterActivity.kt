7e5b05ce28bbcaf92e90c4ae5514bc71fa9fe668
Cuong Phan <vcuong0909@gmail.com>
2021-09-13 15:25:33 -0400

    completed feature

--- app/src/main/java/com/example/groceryshopper/view/RegisterActivity.kt
+++ app/src/main/java/com/example/groceryshopper/view/RegisterActivity.kt
@@ -3,23 +3,31 @@ package com.example.groceryshopper.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.groceryshopper.UrlRequest.BASE_URL
+import com.example.groceryshopper.UrlRequest.REGISTER_END_POINT
 import com.example.groceryshopper.databinding.ActivityRegisterBinding
+import org.json.JSONObject
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestQueue = Volley.newRequestQueue(baseContext)
         setupEvents()
     }
 
     private fun setupEvents() {
         binding.btnSignUp.setOnClickListener {
             val firstName = binding.etFirstName.text.toString()
-            val lastName = binding.etLastName.text.toString()
-            val address = binding.etAddress.text.toString()
             val mobilePhone = binding.etMobilePhone.text.toString()
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
@@ -27,43 +35,51 @@ class RegisterActivity : AppCompatActivity() {
 
             if (firstName.isEmpty()) {
                 binding.etFirstName.error = "Invalid first name"
-            }
-            if (lastName.isEmpty()) {
-                binding.etLastName.error = "Invalid last name"
-            }
-            if (address.isEmpty()) {
-                binding.etAddress.error = "Invalid address"
-            }
-            if (mobilePhone.isEmpty()) {
-                binding.etAddress.error = "Invalid phone number"
+                return@setOnClickListener
             }
             if (email.isEmpty()) {
                 binding.etEmail.error = "Invalid email"
+                return@setOnClickListener
             }
             if (password.isEmpty()) {
                 binding.etPassword.error = "Invalid password"
+                return@setOnClickListener
             }
             if (password != confirmPassword) {
                 binding.etPassword.error = "Password not matched"
                 binding.etConfirmPassword.error = "Password not matched"
+                return@setOnClickListener
             }
 
-            val sharedPref = getSharedPreferences("userAuthentication", MODE_PRIVATE)
-            val editor = sharedPref.edit()
-            editor.putString("firstName", firstName)
-            editor.putString("lastName", lastName)
-            editor.putString("address", address)
-            editor.putString("mobilePhone", mobilePhone)
-            editor.putString("email", email)
-            editor.putString("password", password)
-            editor.apply()
+            // API POST here
+            val userInfo = JSONObject()
+            userInfo.put("firstName", firstName)
+            userInfo.put("mobile", mobilePhone)
+            userInfo.put("password", password)
+            userInfo.put("email", email)
 
-            // API post here
-
+            val registerRequest = JsonObjectRequest(
+                Request.Method.POST,
+                "$BASE_URL$REGISTER_END_POINT",
+                userInfo,
+                {
+                    val error = it.getString("error").toBoolean()
+                    if (error) {
+                        Toast.makeText(baseContext, "Register failed!", Toast.LENGTH_LONG).show()
+                    } else{
+                        Toast.makeText(baseContext, "Register successfully!", Toast.LENGTH_LONG).show()
+                        finish()
+                    }
+                }, {
+                    it.printStackTrace()
+                    Toast.makeText(baseContext, "Cannot complete the register request!", Toast.LENGTH_LONG).show()
+                }
+            )
+            requestQueue.add(registerRequest)
         }
 
         binding.btnAlreadyHaveAnAccount.setOnClickListener{
-            startActivity(Intent(baseContext, LoginActivity::class.java))
+            finish()
         }
     }
 }
